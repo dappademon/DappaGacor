3172814f7d6f61c9ad31f90a53598d6177a05767
dappademon <alfahrizi2006@gmail.com>
2026-05-05 11:56:14 +0700

    gacor1234

--- api/index.php
+++ api/index.php
@@ -1,2 +1,11 @@
 <?php
+// Handle static files
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+if ($uri !== '/' && file_exists(__DIR__ . '/../public' . $uri)) {
+    return false;
+}
+
 require __DIR__ . '/../public/index.php';
